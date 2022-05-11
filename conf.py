4f9fd136f72ea1d073ceea7046f90fa7b83ebbab
Till Hoffmann <tillahoffmann@gmail.com>
2022-05-11 10:53:15 -0400

    Add `intersphinx` extension for cross references.

--- conf.py
+++ conf.py
@@ -4,6 +4,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.napoleon',
     'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
 ]
 project = 'TEMPLATE_NAME'
 napoleon_custom_sections = [('Returns', 'params_style')]
@@ -15,3 +16,11 @@ html_theme = 'nature'
 # Configure autodoc to avoid excessively long fully-qualified names.
 add_module_names = False
 autodoc_typehints_format = 'short'
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'numpy': ('https://numpy.org/doc/stable/', None),
+    'scipy': ('https://docs.scipy.org/doc/scipy/', None),
+    'matplotlib': ('https://matplotlib.org/stable', None),
+    'torch': ('https://pytorch.org/docs/stable/', None),
+}
